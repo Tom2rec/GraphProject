8779bae20a0e0aab500225633afb85de7baad43a
Tom2rec <t.ture2000@gmail.com>
2021-05-31 23:12:42 +0200

    feat: class to represent edge

---
+++ edge.py
@@ -0,0 +1,8 @@
+# class to represent one edge from graph
+
+class Edge:
+
+    def __init__(self, start, end, weight):
+        self.start = start
+        self.end = end
+        self.weight = weight
