e36156e15bcc5970716065332afc5237292b01a4
Tom2rec <t.turek2000@gmail.com>
2021-06-01 15:56:38 +0200

    refactor: change distance variable type

--- Bellman_Ford_algorithm.py
+++ Bellman_Ford_algorithm.py
@@ -3,9 +3,11 @@ from graph import Graph
 # Bellman_ford algorithm implementation for specific graph
 
 
-def bellman_ford(graph: Graph) -> float:
+def bellman_ford(graph: Graph):
     try:
-        distance = [float("Inf")] * graph.verticesCount
+        distance = dict()
+        for i in graph.vertices:
+            distance[i] = float("Inf")
         distance[Graph.source] = 0
 
         for _ in range(graph.verticesCount - 1):
@@ -25,28 +27,28 @@ def bellman_ford(graph: Graph) -> float:
         return 0
 
 
-def print_solution(graph: Graph,result):
+def print_solution(result):
     if result:
         print("")
         print("-" * (len(result) * 8 + 30))
         print("|           VERTEX           |   ", end="")
-        for vertex in graph.vertices:
+        for vertex in result:
             print(f"{vertex}   |   ", end="")
         print("")
         print("-" * (len(result) * 8 + 30))
         print("|  DISTANCE FROM THE SOURCE  |   ", end="")
-        for res in result:
-            if res < 0:
-                print(f"\b{res}   |   ", end="")
+        for key in result:
+            if result[key] < 0:
+                print(f"\b{result[key]}   |   ", end="")
                 continue
-            if res < 10:
-                print(f"{res}   |   ", end="")
+            if result[key] < 10:
+                print(f"{result[key]}   |   ", end="")
                 continue
-            if res < 100:
-                print(f"{res}  |   ", end="")
+            if result[key] < 100:
+                print(f"{result[key]}  |   ", end="")
                 continue
-            if res < 1000:
-                print(f"\b{res}  |   ", end="")
+            if result[key] < 1000:
+                print(f"\b{result[key]}  |   ", end="")
                 continue
         print("")
         print("-" * (len(result) * 8 + 30))
