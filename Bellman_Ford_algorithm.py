a85cecad7029995aa30b8a385ec205e06f68b0ad
Tom2rec <t.ture2000@gmail.com>
2021-06-01 11:04:55 +0200

    feat: exception

--- Bellman_Ford_algorithm.py
+++ Bellman_Ford_algorithm.py
@@ -4,47 +4,52 @@ from graph import Graph
 
 
 def bellman_ford(graph: Graph) -> float:
+    try:
+        distance = [float("Inf")] * graph.verticesCount
+        distance[Graph.source] = 0
 
-    distance = [float("Inf")] * graph.verticesCount
-    distance[Graph.source] = 0
+        for _ in range(graph.verticesCount - 1):
+            for edge in graph.graph:
+                if distance[edge.start] != float("Inf") and distance[edge.start] + edge.weight < distance[edge.end]:
+                    distance[edge.end] = distance[edge.start] + edge.weight
 
-    for _ in range(graph.verticesCount - 1):
         for edge in graph.graph:
             if distance[edge.start] != float("Inf") and distance[edge.start] + edge.weight < distance[edge.end]:
-                distance[edge.end] = distance[edge.start] + edge.weight
-
-    for edge in graph.graph:
-        if distance[edge.start] != float("Inf") and distance[edge.start] + edge.weight < distance[edge.end]:
-            print("Graf zawiera cykl o łącznej ujemnej wadze")
-            return 0
-    return distance
+                print()
+                print("Graph contains negative weight cycle, which is not allowed")
+                return 0
+        return distance
+    except:
+        print()
+        print("JSON file should contain info for each vertex")
+        return 0
 
 
 def print_solution(graph: Graph,result):
-
-    print("")
-    print("-" * (len(result) * 8 + 30))
-    print("|           VERTEX           |   ", end="")
-    for vertex in sorted(graph.vertices):
-        print(f"{vertex}   |   ", end="")
-    print("")
-    print("-" * (len(result) * 8 + 30))
-    print("|  DISTANCE FROM THE SOURCE  |   ", end="")
-    for res in result:
-        if res < 0:
-            print(f"\b{res}   |   ", end="")
-            continue
-        if res < 10:
-            print(f"{res}   |   ", end="")
-            continue
-        if res < 100:
-            print(f"{res}  |   ", end="")
-            continue
-        if res < 1000:
-            print(f"\b{res}  |   ", end="")
-            continue
-    print("")
-    print("-" * (len(result) * 8 + 30))
+    if result:
+        print("")
+        print("-" * (len(result) * 8 + 30))
+        print("|           VERTEX           |   ", end="")
+        for vertex in graph.vertices:
+            print(f"{vertex}   |   ", end="")
+        print("")
+        print("-" * (len(result) * 8 + 30))
+        print("|  DISTANCE FROM THE SOURCE  |   ", end="")
+        for res in result:
+            if res < 0:
+                print(f"\b{res}   |   ", end="")
+                continue
+            if res < 10:
+                print(f"{res}   |   ", end="")
+                continue
+            if res < 100:
+                print(f"{res}  |   ", end="")
+                continue
+            if res < 1000:
+                print(f"\b{res}  |   ", end="")
+                continue
+        print("")
+        print("-" * (len(result) * 8 + 30))
 
 
 
